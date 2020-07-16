b9ae04b7705adcefb09d69fc852ff8929ff33bb7
Haley Workman <haleywrkmn@gmail.com>
2020-07-16 21:41:10 +0800

    Reworked code into many more methods, fixed a few bugs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import PIL, wordcloud, sys, typing, discord, datetime
+import PIL, wordcloud, sys, discord, datetime
 from discord.ext.commands import Bot
 
 PREFIX = "."
@@ -17,52 +17,59 @@ bot = Bot(command_prefix=PREFIX)
     usage = "Time: {num}{d, m, or y} (Default: 6h), This Channel Only: {True/False} (Default: True)"
 )
 
-async def whatdidimiss(ctx, in_time: typing.Optional[str] = "6h", in_one_channel: typing.Optional[str] = "True"):
-    minutes = 6
-    unit = "h"
-    units = ("m", "h", "d")
-    falseValues = ("false", "0", "no")
+async def whatdidimiss(ctx, in_time = "6h", in_one_channel = "True"):
     try:
-        minutes = int(in_time[:-1])
-        unit = in_time[-1].lower()
-        if not unit in units:
-            raise ValueError("Invalid unit")
+        minutes = parse_time_to_minutes(in_time)
+        one_channel = parse_bool(in_one_channel)
+        # Getting the earliest time that should be used
+        timestamp = datetime.datetime.utcnow() - datetime.timedelta(minutes=minutes)
+
+        # And now for the slow stuff
+        async with ctx.typing():
+            # Next, recursively grabbing messages and appending them to a long ass string
+            words = await collect_messages(ctx, one_channel, timestamp)
+            create_wordcloud(words, "wordcloud.png")
+            await ctx.send(file=discord.File(open("wordcloud.png", "rb")))
+    except ValueError as e:
+        await ctx.send(f"Error: {e.message}")
+
+def parse_time_to_minutes(raw_time):
+    units = ("m", "h", "d")
+    try:
+        minutes = int(raw_time[:-1])
     except ValueError:
-        await ctx.send("That's not a valid time >:( assuming 6 hours")
-    
-    # Throwing away any more than the first 2 arguments, and moving on:
+        raise ValueError("Invalid time duration.")
+    unit = raw_time[-1].lower()
+    if not unit in units:
+        raise ValueError("Invalid time unit")
     if unit == "m":
         pass
     elif unit == "d":
         minutes *= 1440
     else:
         minutes *= 60
-    # Default unit is hours
+    return minutes
 
-    one_channel = not in_one_channel.lower() in falseValues
+def parse_bool(in_bool):
+    falseValues = ("false", "0", "no")
+    return in_bool.lower() not in falseValues
 
-    # Getting the earliest time that should be used
-    timestamp = datetime.datetime.utcnow() - datetime.timedelta(minutes=minutes)
+async def collect_messages(ctx, one_channel, timestamp):
+    # Getting the channel's that should be grabbed from
+    if one_channel or ctx.guild is None: # If the message isn't in a server just grab current channel
+        histories = [ctx.history]
+    else:
+        histories = [i.history for i in list(filter(lambda i: type(i) is discord.TextChannel, ctx.guild.channels))]
+    words = ""
+    for hist in histories:
+        words += (" ".join(await hist(limit=None, after=timestamp).map(lambda x: x.content).flatten())) + " "
+    return words[:-1]
 
-    # And now for the slow stuff
-    async with ctx.typing():
-        # Getting the channel's that should be grabbed from
-        if one_channel or ctx.guild is None: # If the message isn't in a server just grab current channel
-            histories = [ctx.history]
-        else:
-            histories = [i.history for i in list(filter(lambda i: type(i) is discord.TextChannel, ctx.guild.channels))]
-        
-        # Next, recursively grabbing messages and appending them to a long ass string
-        words = ""
-        for hist in histories:
-            words += (" ".join(await hist(limit=None, after=timestamp).map(lambda x: x.content).flatten())) + " "
-
-        wc = wordcloud.WordCloud(
-            collocations = False
-        )
-        wc.generate(words).to_file("wordcloud.png")
-
-        await ctx.send(file=discord.File(open("wordcloud.png", "rb")))
+def create_wordcloud(words, filename):
+    wc = wordcloud.WordCloud(
+        collocations = False
+    )
+    wc.generate(words).to_file(filename)
 
 @bot.event
 async def on_ready():
