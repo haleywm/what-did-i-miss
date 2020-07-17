1c7d9fa84660a6eb54249a16a122dd99ecda8132
Haley Workman <haleywrkmn@gmail.com>
2020-07-17 20:00:09 +0800

    Optimised wordcloud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import PIL, wordcloud, sys, discord, datetime
+import PIL, wordcloud, sys, discord, datetime, asyncio
+import concurrent.futures
 from discord.ext.commands import Bot
 
 class UserError(Exception):
@@ -12,6 +13,8 @@ try:
 except FileNotFoundError:
     print("Please create a key.txt file containing the discord bot private key.")
     sys.exit(-1)
+
+wordcloud.STOPWORDS.add("wordcloud")
 
 bot = Bot(command_prefix=PREFIX)
 
@@ -29,10 +32,11 @@ async def whatdidimiss(ctx, in_time = "6h", in_one_channel = "True"):
         timestamp = datetime.datetime.utcnow() - datetime.timedelta(minutes=minutes)
 
         # And now for the slow stuff
-        async with ctx.typing():
+        with ctx.typing():
             # Next, recursively grabbing messages and appending them to a long ass string
             words = await collect_messages(ctx, one_channel, timestamp)
-            create_wordcloud(words, "wordcloud.png")
+            with concurrent.futures.ProcessPoolExecutor() as pool:
+                await asyncio.get_event_loop().run_in_executor(pool, create_wordcloud, words, "wordcloud.png")
             await ctx.send(file=discord.File(open("wordcloud.png", "rb")))
     except UserError as e:
         await ctx.send(f"Invalid Input: {e.message}")
@@ -62,16 +66,28 @@ async def collect_messages(ctx, one_channel, timestamp):
         histories = [ctx.history]
     else:
         histories = [i.history for i in list(filter(lambda i: type(i) is discord.TextChannel, ctx.guild.channels))]
-    words = ""
+    words = dict()
     for hist in histories:
-        words += (" ".join(await hist(limit=None, after=timestamp).map(lambda x: x.content).flatten())) + " "
-    return words[:-1]
+        async for msg in hist(limit=None, after=timestamp):
+            add_frequency(words, msg.content)
+    return words
 
 def create_wordcloud(words, filename):
     wc = wordcloud.WordCloud(
         collocations = False
     )
-    wc.generate(words).to_file(filename)
+    wc.generate_from_frequencies(words).to_file(filename)
+
+def add_frequency(freq_dict, text):
+    # A dictionary of words, each word having an integer value of it's frequency
+    # Adds the frequency to an existing set, pass an empty dict() to start with.
+    for word in text.split():
+        word = word.lower().strip('.,!?\'"')
+        if word not in wordcloud.STOPWORDS:
+            if word in freq_dict:
+                freq_dict[word] += 1
+            else:
+                freq_dict[word] = 1
 
 @bot.event
 async def on_ready():
