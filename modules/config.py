a4ed8dadc74a018946e40159f7a6d9acf3b8af6d
archeoid <aarcheoid@gmail.com>
2020-07-21 20:32:44 +0800

    Discord Emoji Support + debug mode

--- modules/config.py
+++ modules/config.py
@@ -5,7 +5,6 @@ try:
     from yaml import CLoader as Loader
 except ImportError:
     from yaml import Loader
-
 # Loading config data, catching errors in a user friendly way
 try:
     try:
@@ -15,21 +14,22 @@ try:
         sys.exit(1)
     merge_dicts(CONFIG, load(open("config.yml"), Loader=Loader))
 except FileNotFoundError:
-    ## TEMP SETTING MIGRATE CODE, remove in future:
-    # If a key.txt file exists, attempt to migrate it into a config file
-    try:
-        with open("key.txt") as file:
-            with open("config.yml", "w") as output:
-                output.write("key: " + file.readline())
-                print("Migrated key to config.yml")
+    if(not CONFIG["commands"]["whatdidimiss"]["debug"]): #debug doesent need key
+        ## TEMP SETTING MIGRATE CODE, remove in future:
+        # If a key.txt file exists, attempt to migrate it into a config file
         try:
-            merge_dicts(CONFIG, load(open("config.yml"), Loader=Loader))
-        except YAMLError:
-            print("Attempted to generate config.yml from key.txt, encountered invalid output")
+            with open("key.txt") as file:
+                with open("config.yml", "w") as output:
+                    output.write("key: " + file.readline())
+                    print("Migrated key to config.yml")
+            try:
+                merge_dicts(CONFIG, load(open("config.yml"), Loader=Loader))
+            except YAMLError:
+                print("Attempted to generate config.yml from key.txt, encountered invalid output")
+                sys.exit(1)
+        except FileNotFoundError:
+            print("Please create a config.yml file containing the discord bot private key.")
             sys.exit(1)
-    except FileNotFoundError:
-        print("Please create a config.yml file containing the discord bot private key.")
-        sys.exit(1)
 except YAMLError:
     print("Please create a valid config.yml as per the example file, or the README")
     sys.exit(1)
