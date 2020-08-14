efc61088209b69e2f1ef21b50e73afebb493b022
mareau <mareau999@gmail.com>
2020-08-14 23:50:12 +0800

    Fix bug in api_service and add typing indicators to commands

--- modules/cat/api_service.py
+++ modules/cat/api_service.py
@@ -1,6 +1,7 @@
 import io
 import aiohttp
 import mimetypes
+import discord
 
 
 async def get_cat_image(gif=False):
--- modules/cat/command.py
+++ modules/cat/command.py
@@ -12,23 +12,26 @@ class Cat(commands.Cog):
     """
     @bot.group(invoke_without_command=True)
     async def cat(self, ctx):
-        cat = await get_cat_image()
+        async with ctx.typing():
+            cat = await get_cat_image()
 
-        await ctx.send(file=cat)
+            await ctx.send(file=cat)
 
     """ 
     SUB-COMMANDS
     """
     @cat.command()
     async def gif(self, ctx):
-        cat = await get_cat_image(gif=True)
+        async with ctx.typing():
+            cat = await get_cat_image(gif=True)
 
-        await ctx.send(file=cat)
+            await ctx.send(file=cat)
 
     @cat.command()
     async def help(self, ctx):
-        usage_text = f"Usage: {PREFIX}cat [gif]"
-        help_text = "Display a random cat image or gif"
-        embed = discord.Embed(title=self.qualified_name, description=f"{usage_text}\n\n{help_text}")
+        async with ctx.typing():
+            usage_text = f"Usage: {PREFIX}cat [gif]"
+            help_text = "Display a random cat image or gif"
+            embed = discord.Embed(title=self.qualified_name, description=f"{usage_text}\n\n{help_text}")
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
