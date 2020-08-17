9553806c164dea0f32349701f75dcdb00d06220c
haleywm <haleywrkmn@gmail.com>
2020-08-17 14:02:34 +0800

    Added barebones db backend

--- services/database/database.py
+++ services/database/database.py
@@ -4,11 +4,44 @@ from services.database.base import Base
 from services.database.tables.server import Server
 
 # Connecting to the database
-engine = create_engine("sqlite:///:memory:")
+engine = create_engine("sqlite://server_variables.db")
 # The server module registered itself with base when imported, creating missing db structures:
+# TODO: Support for migration using sqlalchemy alembic. Not neccessary until I want to modify table structure in an update
 Base.metadata.create_all(engine)
 
 Session = sessionmaker()
 Session.configure(bind=engine)
 
+def get_server(server_id):
+    "Lookup a server with the given ID and return it. Returns None if server doesn't exist."
+    session = Session()
+    server = session.query(Server).filter(Server.server_id == server_id).first()
+    session.close()
+    return server
 
+def set_server(server_id, newvals):
+    """set_server
+    Updates the given server with the new values listed, and creates a new database entry if needed.
+    Returns the new server value so that it can be used if needed.
+    PARAMETERS
+    ----------
+    server_id (int)
+        The id of the server being set.
+    newvals (dict)
+        The new values that should be set, in key: value format.
+        Values that aren't part of the Server class are discarded.
+    """
+    # Create a new session, check if an item already exists, and either update the server or create a new entry
+    session = Session()
+    server = session.query(Server).filter(Server.server_id == server_id).first()
+    # server is None is doesn't exist
+    if not server:
+        server = Server(server_id = server_id)
+        session.add(server)
+    
+    for key, value in newvals.items():
+        setattr(server, key, value)
+    
+    # Commiting the session saves the changes. As has already been added to session, it has been keeping track of changes for us.
+    session.commit()
+    return server
--- services/database/tables/server.py
+++ services/database/tables/server.py
@@ -5,11 +5,11 @@ class Server(Base):
     __tablename__ = 'servers'
 
     # The server ID
-    id = Column(Integer, primary_key=True)
+    server_id = Column(Integer, primary_key=True, autoincrement=False)
     # The ID of the channel that is used for bot commands
     bot_channel = Column(Integer)
     # The list of commands that are 'bot channel only'. If empty, all commands are allowed.
     bot_channel_commands = Column(String)
 
     def __repr__(self):
-        return f"<Server(id={self.id},bot_channel={self.bot_channel},bot_channel_commands='{self.bot_channel_commands}')>"
+        return f"<Server(id={self.server_id},bot_channel={self.bot_channel},bot_channel_commands='{self.bot_channel_commands}')>"
